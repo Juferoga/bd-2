a9e38a023e082380ff615ba74296eff314fab709
Juferoga <juancholoco2000.61@gmail.com>
2023-02-26 23:16:53 -0500

    JR - frontend project advance
    TODO: determine fields of forms, add integrants to front

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,21 +6,58 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LayoutComponent } from './layout/layout.component';
 import { LoginComponent } from './pages/login/login.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { AdminComponent } from './pages/admin/admin.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { HttpClientModule } from '@angular/common/http';
+import {InputTextModule} from 'primeng/inputtext';
+import {PasswordModule} from 'primeng/password';
+import {ButtonModule} from 'primeng/button';
+import {ToastModule} from 'primeng/toast';
+import {MessageService} from 'primeng/api';
+import {MenubarModule} from 'primeng/menubar';
+import { TopBarComponent } from './shared/top-bar/top-bar.component';
+import { SideBarComponent } from './shared/side-bar/side-bar.component';
+import {SlideMenuModule} from 'primeng/slidemenu';
+import {MenuModule} from 'primeng/menu';
+import { ProfileComponent } from './shared/profile/profile.component';
+import {ChartModule} from 'primeng/chart';
+import { OnBuildComponent } from './pages/on-build/on-build.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LayoutComponent,
-    LoginComponent
+    LoginComponent,
+    AdminComponent,
+    HomeComponent,
+    NotFoundComponent,
+    TopBarComponent,
+    SideBarComponent,
+    ProfileComponent,
+    OnBuildComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    InputTextModule,
+    PasswordModule,
+    ButtonModule,
+    HttpClientModule,
+    ToastModule,
+    FormsModule,
+    MenubarModule,
+    SlideMenuModule,
+    MenuModule,
+    ChartModule,
   ],
-  providers: [],
+  providers: [
+    MessageService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ frontend/src/environments/environment.development.ts
@@ -0,0 +1,16 @@
+export const environment = {
+  docs:'https://juferoga.github.io/bd-2',
+  cronograma: 'https://github.com/users/Juferoga/projects/3/views/3',
+  repositorio: 'https://github.com/juferoga/bd-2',
+  juferoga: 'https://github.com/juferoga/',
+  integrantes: [
+    {'name':'Andres Baquero','code':'2018102000','url':'https://github.com/juferoga'},
+    {'name':'Bryan Muñoz','code':'20171020000','url':'https://github.com/juferoga'},
+    {'name':'Cristian Yara','code':'20181020000','url':'https://github.com/juferoga'},
+    {'name':'Johan Cortes','code':'20181020000','url':'https://github.com/juferoga'},
+    {'name':'Juan Felipe Rodríguez','code':'20181020158','url':'https://github.com/juferoga'},
+  ],
+  materia: 'BD-2',
+  proyecto: 'Ventas multi nivel',
+  api: 'localhost:8000/'
+};
