1d0c1affb5e9bcd145f9b07571d0698c7cd85cec
Juferoga <juferoga@gmail.com>
2023-03-12 22:29:55 -0500

    JR - Clean code and finish feature my stock

---
+++ frontend/src/app/shared/my-referrals/my-referrals.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-referrals',
+  templateUrl: './my-referrals.component.html',
+  styleUrls: ['./my-referrals.component.scss']
+})
+export class MyReferralsComponent {
+
+}
