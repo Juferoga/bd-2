1089e56ab94b64b16fe6193d1667a1c74ce5a7da
Bryan Muñoz <swsbmm@gmail.com>
2023-03-18 05:42:41 -0500

    BM. add() Comentarios y ajuste de codigo.

--- backend/ventas_multinivel_api/api/views.py
+++ backend/ventas_multinivel_api/api/views.py
@@ -6,31 +6,48 @@ from rest_framework_simplejwt.tokens import AccessToken
 from rest_framework_simplejwt.tokens import RefreshToken
 
 
-
+# clase del endpoint /api/login 
 class UserLogin(APIView):
     
+    # Metodo post de /api/login
     def post(self, request):
         # Obtén las credenciales de la base de datos del cuerpo de la solicitud
         username = request.data.get('username')
         password = request.data.get('password')
 
+        # Se autentica el usuario ante ORACLE.
         success, connection = open_connection(username,password)
+        connection.close()
+        # Si se realiza la conexion exitosamente el usuario es un usuario de la base de datos.
         if success :
             try :
+                # Se autentica el usuario ante DJANGO.
                 user = authenticate(username=username, password=password)
-                access_token = AccessToken.for_user(user=user)
-                refresh_token = RefreshToken.for_user(user=user)
-
-                connection.close()
+                # Se verifica si el usuario esta autenticado
+                if(user.is_authenticated):
+                    # Se generan los JWT.
+                    access_token = AccessToken.for_user(user=user)
+                    refresh_token = RefreshToken.for_user(user=user)
+                else:
+                    # En caso de que no este autenticado envia un mensaje.
+                    return Response(
+                    {
+                        {"message": "user is not authenticate in DJANGO: %s" % username}
+                    }, status=200)
+                
+                
+                # Si finalmente todo es correcto, devuelve el JWT access y refresh.
                 return Response(
                     {
                         'access': str(access_token),
                         'refresh': str(refresh_token)
                     }, status=200)
             except Exception as e :
+                # Si algo sale mal en la autenticacion, generacion de tokens o en la respuesta se captura el error para su interpretacion.
                 data = {"message": str(e)}
                 return Response(data)
         else :
+            # Cualquier error presentado en la conexion a la BD sale de la siguiente linea.
             data = {"message": str(connection)}
             return Response(data)
         
