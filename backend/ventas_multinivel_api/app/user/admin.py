739a04669b14b0d4ff4163a941bfafee1fde6939
Juferoga <juferoga@gmail.com>
2023-03-30 18:27:32 -0500

    fix error

--- backend/ventas_multinivel_api/app/user/admin.py
+++ backend/ventas_multinivel_api/app/user/admin.py
@@ -3,13 +3,12 @@ from app.user.models import User
 from django.contrib.auth import get_user_model
 @admin.register(get_user_model())
 class UserAdmin(admin.ModelAdmin):
-  readonly_fields = ('email')
   list_display = ('username', 'role', 'oracle_password', 'is_superuser', 'is_staff')
   fieldsets = (
     (
       'Información básica', 
       {
-        'fields': ('username', 'email', 'password', 'role', 'uid', 'oracle_password')
+        'fields': ('username', 'email', 'password', 'role', 'uid')
       }
     ),
     (
