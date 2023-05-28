97a7dc13aec362ff230f4314839e377e229b7fc8
Juferoga <juancholoco2000.61@gmail.com>
2023-05-28 01:29:54 -0500

    Minimal changes

--- frontend/src/app/pages/purchase/purchase.component.ts
+++ frontend/src/app/pages/purchase/purchase.component.ts
@@ -7,22 +7,4 @@ import { Component } from '@angular/core';
 })
 export class PurchaseComponent {
 
-  products:any;
-
-  getSeverity (product) {
-    switch (product.inventoryStatus) {
-        case 'INSTOCK':
-            return 'success';
-
-        case 'LOWSTOCK':
-            return 'warning';
-
-        case 'OUTOFSTOCK':
-            return 'danger';
-
-        default:
-            return null;
-    }
-};
-
 }
